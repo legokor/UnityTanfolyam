f92f4bb3fd95a3e888ac557b5516e758a9929b73
Czeglédi Krisztián <czegledi.krisz@edu.bme.hu>
2025-02-23 22:35:47 +0100

    1. Alkalom solution

--- Assets/1. Alkalom/Scripts/DebugTest.cs
+++ Assets/1. Alkalom/Scripts/DebugTest.cs
@@ -4,12 +4,19 @@ public class DebugTest : MonoBehaviour {
     int i = 1;
 
     //Ide írd a Start függvényt, amely kiírja figyelmeztetés konzolra, hogy "Hello World!"
-        
+    void Start(){
+        Debug.LogWarning("Hello World!");
+    }
     //
 
 
     //Ide írd az Update függvényt, amely kiírja információ konzolra 1-től 10-ig a számokat!
-        
+    void Update(){
+        if (i <= 10){
+            Debug.Log(i);
+            i++;
+        }
+    }
     //
 
 
