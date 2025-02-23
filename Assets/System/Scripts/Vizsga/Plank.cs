271f62173df8752450b876ddfc1ba21b8d137fd1
Czeglédi Krisztián <czegledi.krisz@edu.bme.hu>
2025-02-23 21:40:21 +0100

    Vizsga Libikóka feladat remegés fix

--- Assets/System/Scripts/Vizsga/Plank.cs
+++ Assets/System/Scripts/Vizsga/Plank.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Plank : MonoBehaviour
 {
+    [SerializeField] GameObject weight;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,5 +14,15 @@ public class Plank : MonoBehaviour
         rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
         rb.centerOfMass = new Vector3(0, -1.5f, 0);
     }
+
+    void Update()
+    {
+        if (weight.GetComponent<Rigidbody>() != null && weight.GetComponent<Rigidbody>().mass >= 150)
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            if (rb == null) return;
+            if (transform.rotation.eulerAngles.x <= 344 && transform.rotation.eulerAngles.x >= 340) rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+    } 
 
 }
